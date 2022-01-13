96a4cca1a9101fcbb5881b63158fa7419d523aa8
Marco <79803313+nnmarcoo@users.noreply.github.com>
2022-01-13 09:29:08 -0500

    Changed input system, changed colorizer

--- main.py
+++ main.py
@@ -3,18 +3,7 @@ from PIL import Image
 from blessed import Terminal
 t = Terminal()
 
-def colorize(r,g,b):
-  vals = {'black': (0,0,0), 'red': (255,0,0), 'green': (0,255,0), 'yellow': (255,255,0), 'blue': (0,0,255), 'magenta': (255,0,255), 'cyan': (0,255,255), 'white': (255,255,255)}
-  match = ''
-  dist = 256
-  for i in vals:
-    check = abs(vals[i][0] - r) + abs(vals[i][1] - g) + abs(vals[i][2] - b)
-    if check < dist:
-      dist = check
-      match = i
-  return match
-
-def img2ascii(img, scale):
+def img2ascii(img, scale, bg, c):
   ch = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
   img = Image.open(img) 
   img = img.convert('RGB')
@@ -28,9 +17,28 @@ def img2ascii(img, scale):
     for x in range(0,w,skipx): #how many characters per line
       r,g,b = p[x,y][0], p[x,y][1], p[x,y][2]
       lum = int(((0.2126*r + 0.7152*g + 0.0722*b) / 256) * 70)
-      #print(ch[lum], end='')
-      colorize(r,g,b,ch[lum])
+      #print(col(ch[lum]), end='')
+      print(ch[lum], end='')
+      #colorize(r,g,b,ch[lum])
       #print(t.black(ch[lum]), end='')
+      if bg == 'W':
+        if c in 'YT':
+          col = t.color_rgb(r,g,b)
+          print(t.on_white(col(ch[lum])),end='')
+        else:
+          print(ch[lum],end='')
+      elif bg == 'B':
+        if c in 'YT':
+          col = t.color_rgb(r,g,b)
+          print(t.on_black(col(ch[lum])),end='')
+        else:
+          print(ch[lum],end='')
+      else:
+        if c in 'YT':
+          col = t.color_rgb(r,g,b)
+          print(col(ch[lum]),end='')
+        else:
+          print(ch[lum],end='')
     print()
 
 def clear():
@@ -40,29 +48,31 @@ def clear():
   os.system(command)
 
 def main():
-  print(colorize(5,255,10))
   ask = True
   while ask:
-      getFile = True
-      while getFile:
-          file = input('Enter a file to convert to ASCII: ')
-          if os.path.isfile(file) :
-            scale = int(input('Enter the scale of the ASCII image (1-100): '))
-            if scale < 101 and scale > 0:
-              img2ascii(file, scale)
-              getFile = False
-          else:
-              print(file + ' is not a valid file.')
-          askAgain = True
-      while askAgain:
-          response = input('Do you want to enter another file? Y/YES/N/NO ==> ')
-          response = response.upper()
-          if response in ("Y", "YES"):
-              clear()
-              askAgain = False
-          elif response in ("N", "NO"):
-              askAgain = False
-              ask = False
+    getImg = True
+    while getImg:
+      img = input('Enter an image to convert to ASCII: ')
+      if os.path.isfile(img):
+        getImg = False
+    getScale = True
+    while getScale:
+      scale = input('Enter scale [1-100]: ')
+      if scale.isnumeric():
+        if int(scale) < 101 and int(scale) > 0:
+          getScale = False
+    getBGC = True
+    while getBGC:
+      BGC = input('Enter background color [W/B/NA]: ').upper()
+      if BGC in 'WBNA':
+        getBGC = False
+    getColor = True  
+    while getColor:
+      isColor = input('Enter color preference [Y/N/T/F]: ').upper()
+      if isColor in 'YNTF':
+        getColor = False
+    img2ascii(img, int(scale), BGC, isColor)
+
 
 
 if __name__ == '__main__':
