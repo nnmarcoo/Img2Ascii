db27e4b8aace69d6a56565bef462a3aba1d7286e
Marco <marcosteam7@gmail.com>
2022-01-16 14:32:13 -0500

    Fixed a ton of bugs, actually functional now

--- main.py
+++ main.py
@@ -4,53 +4,66 @@ from blessed import Terminal
 from alive_progress import alive_bar
 t = Terminal()
 
-# https://code-maven.com/create-images-with-python-pil-pillow
+def toTxt():
+  pass
+
+def toImg(img, scale, bg, c, ch):
+  up = 10 
+  fntsize = 15
+  downscale = img.resize((int(img.width*(scale/100)), int(img.height*(scale/100))))
+  upscale = downscale.resize((downscale.width*up, downscale.height*up))
+  i = upscale.load()
+  w,h = upscale.width,upscale.height
+
+  if bg == 'B':
+      asciiimg = Image.new('RGB', (w, h), color = 'black')
+  elif bg == 'W':
+    asciiimg = Image.new('RGB', (w, h), color = 'white')
+  else:
+    asciiimg = upscale.filter(ImageFilter.BoxBlur(25))
+    darkness = ImageEnhance.Brightness(asciiimg)
+    asciiimg = darkness.enhance(.4)
+  fnt = ImageFont.truetype('Arial.ttf', fntsize)
+  d = ImageDraw.Draw(asciiimg)
+
+  with alive_bar(int((w/fntsize)*(h/fntsize)), bar='filling', spinner='vertical') as bar:
+    for y in range(0,h,fntsize):
+      for x in range(0,w,fntsize):
+        r,g,b = i[x,y][0], i[x,y][1], i[x,y][2]
+        lum = int(((0.2126*r + 0.7152*g + 0.0722*b) / 256) * 70)
+        if c == 'Y':
+          d.text((x,y), ch[::-1][lum], font=fnt, fill=(r, g, b))
+        else:
+          if bg == 'W':
+            d.text((x,y), ch[::-1][lum], font=fnt, fill=(0, 0, 0))
+          else:
+            d.text((x,y), ch[::-1][lum], font=fnt, fill=(255, 255, 255))
+        bar()
+    asciiimg.save('ASCIIoutput.png')
 
 def img2ascii(img, scale, bg, c, format):
   ch = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
   img = Image.open(img) 
   img = img.convert('RGB')
   p = img.load()
-  upscale = 15
-
   w,h = img.width,img.height # get width and height of image
   skipx = int(w / (w * (scale/100)))
   skipy = int(h / (h * (scale/100)) * 2.3)
 
   if format == 'I':
-    if bg == 'B':
-      asciiimg = Image.new('RGB', (w*upscale, h*upscale), color = 'black')
-    elif bg == 'W':
-      asciiimg = Image.new('RGB', (w*upscale, h*upscale), color = 'white')
-    else:
-      asciiimg = img.resize((w*upscale, h*upscale)).filter(ImageFilter.BoxBlur(75))
-      darkness = ImageEnhance.Brightness(asciiimg)
-      asciiimg = darkness.enhance(.5)
-
-    fnt = ImageFont.truetype('Arial.ttf', int((upscale * 3) / 1.5))
-    d = ImageDraw.Draw(asciiimg)
+    toImg(img, scale, bg, c, ch)
   elif format == 'T':
-    if os.path.isfile('ASCIIoutput.txt'):
-      os.remove('ASCIIoutput.txt')
-    txt = open('ASCIIoutput.txt', 'w')
+    toTxt()
 
-    
-  with alive_bar((h//skipy)*(w//skipx), bar='filling', spinner='vertical') as bar:
-    for y in range(0,h,skipy): # how many line
-      for x in range(0,w,skipx): #how many characters per line
-        bar()
-        r,g,b = p[x,y][0], p[x,y][1], p[x,y][2]
-        lum = int(((0.2126*r + 0.7152*g + 0.0722*b) / 256) * 70)
-        if format == 'I':
-          d.text((x*upscale,y*upscale), ch[::-1][lum], font=fnt, fill=(r, g, b))
-        if format == 'T':
-          txt.write(ch[lum])
-      if format == 'T':
-        txt.write('\n')
+
+  #elif format == 'T':
+    #if os.path.isfile('ASCIIoutput.txt'):
+     # os.remove('ASCIIoutput.txt')
+ #   txt = open('ASCIIoutput.txt', 'w')
       
         
-  if format == 'I':
-    asciiimg.save('pil_text_font.png')
+  #if format == 'I':
+    #asciiimg.save('pil_text_font.png')
 
 def clear():
   command = 'clear'
